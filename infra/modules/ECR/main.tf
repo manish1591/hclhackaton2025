9831ac5749f9a865167a5d69e1037d6f95cc1b50
manish1591 <manishredkar108@gmail.com>
2025-03-01 16:51:37 +0530

    Update main.tf

--- infra/modules/ECR/main.tf
+++ infra/modules/ECR/main.tf
@@ -14,3 +14,38 @@ resource "aws_subnet" "public" {
   availability_zone = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true
 }
+
+module "vpc" {
+  source = "./modules/ECR"
+}
+
+# Security Group
+resource "aws_security_group" "web" {
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instances
+resource "aws_instance" "web" {
+  ami           = "ami-0c1a7f89451184c8b"
+  instance_type = "t2.micro"
+  subnet_id     = element(module.vpc.public_subnets, 0)
+  security_groups = [aws_security_group.web.name]
+
+  tags = {
+    Name = "Web Server"
+  }
+}
