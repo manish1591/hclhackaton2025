47ee59d3e42cabac999c286652da626f5b9c1377
manish1591 <manishredkar108@gmail.com>
2025-03-01 16:48:50 +0530

    Update main.tf

--- infra/modules/ECR/main.tf
+++ infra/modules/ECR/main.tf
@@ -1,3 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
 resource "aws_ecr_repository" "app_repo" {
   name = var.repo_name
+}
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
 }
