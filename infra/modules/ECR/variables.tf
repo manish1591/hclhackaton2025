67c9ba0143a210dd1e79f3a66ffe22db0f3255ab
manish1591 <manishredkar108@gmail.com>
2025-03-01 17:00:57 +0530

    Update variables.tf

--- infra/modules/ECR/variables.tf
+++ infra/modules/ECR/variables.tf
@@ -14,3 +14,18 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  subnet_id              = element(var.subnet_ids, count.index % length(var.subnet_ids))
+  vpc_security_group_ids = var.security_group_ids
+
+  tags = {
+    Name        = "${var.environment}-machine-${count.index + 1}"
+    Environment = var.environment
+  }
+variable "ami_id" {
+  description = "AMI ID for the instances"
+
+
